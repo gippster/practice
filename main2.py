2665c9cf02f2ee1723c981a762e6a25b0b864cdd
Asain <asain.baitasov@mail.ru>
2023-06-27 14:52:27 +0600

    add

--- main2.py
+++ main2.py
@@ -1,1 +1,8 @@
 import pandas as pd
+
+df = pd.read_csv('dataset.csv')
+print(df)
+df.info()
+df['datetime'] = pd.to_datetime(df['datetime'], format="%Y-%m-%d %H:%M:%S")
+print(df['datetime'])
+df.info()
