adfc3e7e20fc33d8efda6538773feec8216cd0b9
Asain <asain.baitasov@mail.ru>
2023-06-29 11:13:18 +0600

    update

--- main2.py
+++ main2.py
@@ -1,51 +1,49 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-
 data = pd.read_csv('powerconsumption.csv')
 print(data.head())
 data.info()
-data['datetime'] = pd.to_datetime(data['datetime'])
+data['Datetime'] = pd.to_datetime(data['Datetime'])
 #график изменения электрического потребления со временем
+
 plt.figure(figsize=(12, 6))
 plt.plot(data['Datetime'], data['PowerConsumption'])
-plt.title('Power Consumption over Time')
+plt.title('Electrical Consumption over Time')
 plt.xlabel('Datetime')
-plt.ylabel('Power Consumption (MWh)')
+plt.ylabel('Electricity Consumption (MWh)')
 plt.show()
+
 #распределение температуры
+
 plt.figure(figsize=(8, 6))
 sns.histplot(data['Temperature'], bins=20, kde=True)
 plt.title('Temperature Distribution')
 plt.xlabel('Temperature')
 plt.ylabel('Count')
 plt.show()
-#распределение значений для "var1"
+
+
+#гистограмма для "Humidity"
+
 plt.figure(figsize=(8, 6))
 sns.histplot(data['Humidity'], bins=20, kde=True)
 plt.title('Humidity Distribution')
 plt.xlabel('Humidity')
 plt.ylabel('Count')
 plt.show()
-#гистограмма для "pressure"
-plt.figure(figsize=(8, 6))
-sns.histplot(data['GeneralDiffuseFlows'], bins=20, kde=True)
-plt.title('General Diffuse Flows Distribution')
-plt.xlabel('General Diffuse Flows')
-plt.ylabel('Count')
-plt.show()
 
 plt.figure(figsize=(8, 6))
 sns.histplot(data['WindSpeed'], bins=20, kde=True)
-plt.title('Wind speed Distribution')
-plt.xlabel('Wind speed')
+plt.title('Windspeed Distribution')
+plt.xlabel('WindSpeed')
 plt.ylabel('Count')
 plt.show()
 
 plt.figure(figsize=(8, 6))
-sns.histplot(data['DiffuseFlows'], bins=20, kde=True)
-plt.title('Diffuse Flows Distribution')
-plt.xlabel('Diffuse Flows')
+sns.histplot(data['GeneralDiffuseFlows'], bins=20, kde=True)
+plt.title('GeneralDiffuseFlows Distribution')
+plt.xlabel('GeneralDiffuseFlows')
 plt.ylabel('Count')
 plt.show()
 
@@ -69,5 +67,5 @@ plt.figure(figsize=(8, 6))
 plt.scatter(data['Temperature'], data['PowerConsumption'])
 plt.title('Temperature vs Power Consumption')
 plt.xlabel('Temperature')
-plt.ylabel('Power Consumption')
+plt.ylabel('Power Consumption (MWh)')
 plt.show()
