8bf34b8b5228bc1f341bd86bf94badb94e0a8a03
sajbu <sajbullin77@gmail.com>
2023-06-29 11:07:47 +0600

    update date

--- main.py
+++ main.py
@@ -3,85 +3,71 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from pandas_profiling import ProfileReport
 
-data = pd.read_csv('dataset.csv')
+data = pd.read_csv('powerconsumption.csv')
 print(data.head())
 data.info()
-data['datetime'] = pd.to_datetime(data['datetime'])
-# data.drop(columns=['hour','day_of_month','day_of_week','month',
-#                    'is_weekend','pressure_at_sea','precip_dur_past10min'],axis=1,inplace=True)
-# data.info()
+data['Datetime'] = pd.to_datetime(data['Datetime'])
 #график изменения электрического потребления со временем
 
-# plt.figure(figsize=(12, 6))
-# plt.plot(data['datetime'], data['electricity_consumption'])
-# plt.title('Electrical Consumption over Time')
-# plt.xlabel('Datetime')
-# plt.ylabel('Electricity Consumption (MWh)')
-# plt.show()
+plt.figure(figsize=(12, 6))
+plt.plot(data['Datetime'], data['PowerConsumption'])
+plt.title('Electrical Consumption over Time')
+plt.xlabel('Datetime')
+plt.ylabel('Electricity Consumption (MWh)')
+plt.show()
 
-# #распределение температуры
+#распределение температуры
 
-# plt.figure(figsize=(8, 6))
-# sns.histplot(data['temperature'], bins=20, kde=True)
-# plt.title('Temperature Distribution')
-# plt.xlabel('Temperature')
-# plt.ylabel('Count')
-# plt.show()
+plt.figure(figsize=(8, 6))
+sns.histplot(data['Temperature'], bins=20, kde=True)
+plt.title('Temperature Distribution')
+plt.xlabel('Temperature')
+plt.ylabel('Count')
+plt.show()
 
-# #распределение значений для "var1"
 
-# plt.figure(figsize=(8, 6))
-# sns.histplot(data['var1'], bins=20, kde=True)
-# plt.title('Var1 Distribution')
-# plt.xlabel('Var1')
-# plt.ylabel('Count')
-# plt.show()
+#гистограмма для "Humidity"
 
-# #гистограмма для "pressure"
+plt.figure(figsize=(8, 6))
+sns.histplot(data['Humidity'], bins=20, kde=True)
+plt.title('Humidity Distribution')
+plt.xlabel('Humidity')
+plt.ylabel('Count')
+plt.show()
 
-# plt.figure(figsize=(8, 6))
-# sns.histplot(data['pressure'], bins=20, kde=True)
-# plt.title('Pressure Distribution')
-# plt.xlabel('Pressure')
-# plt.ylabel('Count')
-# plt.show()
+plt.figure(figsize=(8, 6))
+sns.histplot(data['WindSpeed'], bins=20, kde=True)
+plt.title('Windspeed Distribution')
+plt.xlabel('WindSpeed')
+plt.ylabel('Count')
+plt.show()
 
-# plt.figure(figsize=(8, 6))
-# sns.histplot(data['windspeed'], bins=20, kde=True)
-# plt.title('Windspeed Distribution')
-# plt.xlabel('Windspeed')
-# plt.ylabel('Count')
-# plt.show()
+plt.figure(figsize=(8, 6))
+sns.histplot(data['GeneralDiffuseFlows'], bins=20, kde=True)
+plt.title('GeneralDiffuseFlows Distribution')
+plt.xlabel('GeneralDiffuseFlows')
+plt.ylabel('Count')
+plt.show()
 
-# plt.figure(figsize=(8, 6))
-# sns.histplot(data['var2'], bins=20, kde=True)
-# plt.title('Var2 Distribution')
-# plt.xlabel('Var2')
-# plt.ylabel('Count')
-# plt.show()
+plt.figure(figsize=(8, 6))
+sns.histplot(data['PowerConsumption'], bins=20, kde=True)
+plt.title('Power Consumption Distribution')
+plt.xlabel('Power Consumption (MWh)')
+plt.ylabel('Count')
+plt.show()
 
-# plt.figure(figsize=(8, 6))
-# sns.histplot(data['electricity_consumption'], bins=20, kde=True)
-# plt.title('Electricity Consumption Distribution')
-# plt.xlabel('Electricity Consumption (MWh)')
-# plt.ylabel('Count')
-# plt.show()
+# Вычисление корреляционной матрицы
+correlation_matrix = data.corr()
 
-# # Вычисление корреляционной матрицы
-# correlation_matrix = data.corr()
+# Визуализация корреляций с помощью тепловой карты
+plt.figure(figsize=(10, 8))
+sns.heatmap(correlation_matrix, annot=True, cmap="coolwarm")
+plt.title('Correlation Matrix')
+plt.show()
 
-# # Визуализация корреляций с помощью тепловой карты
-# plt.figure(figsize=(10, 8))
-# sns.heatmap(correlation_matrix, annot=True, cmap="coolwarm")
-# plt.title('Correlation Matrix')
-# plt.show()
-
-# plt.figure(figsize=(8, 6))
-# plt.scatter(data['windspeed'], data['electricity_consumption'])
-# plt.title('windspeed vs Electricity Consumption')
-# plt.xlabel('windspeed')
-# plt.ylabel('Electricity Consumption (MWh)')
-# plt.show()
-
-profile = ProfileReport(data)
-print(profile)
+plt.figure(figsize=(8, 6))
+plt.scatter(data['windspeed'], data['electricity_consumption'])
+plt.title('windspeed vs Electricity Consumption')
+plt.xlabel('windspeed')
+plt.ylabel('Electricity Consumption (MWh)')
+plt.show()
